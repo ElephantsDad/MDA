a3f41ecd14eaa4fc4e5e682edbb428305c6c981c
ElephantsDad <elephantsdadshusband@gmail.com>
2018-11-20 21:09:41 +1000

    Add forms.py, changed spyfall.py, templates/, main.css

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class SubmitForm(FlaskForm):
+    username = StringField('Никнейм:',
+                            validators=[DataRequired(),Length(min=2, max=20)])
+    submit = SubmitField('Начать игру')
--- spyfall.py
+++ spyfall.py
@@ -1,24 +1,34 @@
 #C:\Users\Asus\Projects\Environments\spyfall\Scripts\activate.bat
 #cd C:\Users\Asus\Desktop\spyfall
 from flask import Flask, render_template, url_for, flash, redirect
+from forms import SubmitForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '46adde7c15c23ac027ed4ba9d69ee4d2'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    form = SubmitForm()
+    if form.validate_on_submit():
+        return game()
+    else:
+        return render_template('home.html', form=form)
 
 @app.route("/rules")
 def rules():
-    return render_template('rules.html')
+    return render_template('rules.html', title="Правила игры")
 
 @app.route("/locations")
 def locations():
-    return render_template('locations.html')
+    return render_template('locations.html', title="Локации")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="О разработчиках")
+
+@app.route("/game")
+def game():
+    return render_template('game.html', title="Игра")
 
 if __name__ == '__main__':
     app.run(debug=True)
